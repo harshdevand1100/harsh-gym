5046ff017d0fc5a3e432be14a56fd3cd1f0be81c
shubham <londheshubham68@gmail.com>
2021-05-11 04:25:26 +0530

    files added

---
+++ src/components/home.jsx
@@ -0,0 +1,145 @@
+import React from 'react'
+
+const Home = () =>{
+    return(
+        <div>
+            <h1>Hello!</h1>
+        </div>
+    )
+}
+
+export default Home
+
+// const style = {
+//
+//
+// .left {
+//     display: inline;
+//     /* border: 2px solid red; */
+//     color: white;
+//     position: absolute;
+//     left: 38px;
+//     top: 18px
+// }
+//
+// .left img {
+//     width: 55px;
+// }
+//
+// .left div {
+//     text-align: center;
+//     font-family: 'Times New Roman', Times, serif;
+//     line-height: 1px;
+//     font-family: 'Baloo Tamma 2', cursive;
+//
+// }
+//
+// .mid {
+//     border: 5px solid green;
+//     border-radius: 20px;
+//     color: white;
+//     display: block;
+//     width: 36%;
+//     margin: 18px auto;
+// }
+//
+// .right {
+//     /* border: 2px solid yellow; */
+//     color: white;
+//     display: inline;
+//     position: absolute;
+//     right: 38px;
+//     top: 18px;
+// }
+//
+// .navbar {
+//     display: inline-block;
+// }
+//
+// .navbar li {
+//     color: white;
+//     display: inline-block;
+//     font-size: 20px;
+// }
+//
+// .navbar li a {
+//     color: white;
+//     text-decoration: none;
+//     padding: 56px 12px;
+//
+// }
+//
+// .navbar li a:hover,
+// .navbar li a.active {
+//     color: rgb(120, 115, 115);
+//     text-decoration: undreline;
+//
+//
+// }
+//
+// .btn {
+//     margin: 0px 8px;
+//     background-color: black;
+//     color: white;
+//     padding: 4px 13px;
+//     border-radius: 10px;
+//     font-size: 18px;
+//     cursor: pointer;
+//     font-family: 'Baloo Tamma 2', cursive;
+// }
+//
+// .btn:hover {
+//     background-color: rgb(76, 74, 74);
+// }
+//
+// .container {
+//
+//     margin: 86px 45px;
+//     padding: 56;
+//     width: 32%;
+//     border-radius: 22px;
+//     text-emphasis-color: white;
+// }
+//
+// .form-group input {
+//     font-family: 'Baloo Tamma 2', cursive;
+//     text-align: center;
+//     display: block;
+//     border: 2px solid rgb(9, 71, 19);
+//     width: 378px;
+//     padding: 1px;
+//     margin: 11px auto;
+//     border-radius: 8px;
+//     font-size: 17px;
+//
+// }
+//
+// .container h1 {
+//     text-align: center;
+// }
+//
+// .container button {
+//     display: block;
+//     margin: auto;
+//     width: 44%;
+//     display: block;
+//     margin: auto;
+//     width: 44%;
+//     font-family: 'Baloo Tamma 2', cursive;
+//     text-align: center;
+//     display: block;
+//     border: 2px solid rgb(10, 1, 1);
+//     width: 378px;
+//     padding: 1px;
+//     margin: 11px auto;
+//     border-radius: 8px;
+//     font-size: 17px;
+//     background-color: rgb(223, 149, 149);
+//
+// }
+//
+//
+//
+//
+//
+// }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const __html = require('../public/index.html');
+const template = { __html: __html };
+
+React.module.exports = React.createClass({
+    render: function() {
+        return(
+            <div dangerouslySetInnerHTML={template} />
+        );
+    }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
